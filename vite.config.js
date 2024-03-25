ba3c67f3c8ac6b34d1ad5046e5f54470c0aaf3b6
Stephan Ullmann <stephan.ullmann@posteo.de>
2024-03-25 17:33:09 +0100

    yolo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://stephanullmann.github.io/gh-actions-demo/',
   plugins: [react()],
   test: {
     globals: true,
